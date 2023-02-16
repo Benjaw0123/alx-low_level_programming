6289a5ea5ea5acd865137d601bca355850c318d1
Binyam Major <binyammajor@yahoo.com>
2023-02-16 07:55:37 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A program that print lines using the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}

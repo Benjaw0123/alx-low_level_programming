d1780e84d57f66488d9621b7d55a7ff005a0afdd
Binyam Major <binyammajor@yahoo.com>
2023-02-22 12:55:29 +0300

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the sum of all multiples of 3 or 5 upto 1024
- * Return: Always 0 (Success)
+ * Return: Alwaysi (Success)
  */
 
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 
 	while (i < 1024)
 	{
-	if ((num % 3 == 0) || (num % 5 == 0))
+	if ((i % 3 == 0) || (i % 5 == 0))
 	{
 	z += i;
 	}

1bfc43030a573ac5705163294ce6e2cb275bb2fe
Binyam Major <binyammajor@yahoo.com>
2023-03-20 09:56:47 +0300

    File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it
+ *        is compiled from.
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+

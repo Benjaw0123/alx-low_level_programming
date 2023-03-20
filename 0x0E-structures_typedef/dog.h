797f9ffc3c19ffdad2a0042e99801dac6dbbc7aa
Binyam Major <binyammajor@yahoo.com>
2023-03-20 11:54:29 +0300

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * main - Define a new type named struct dog with elemnts below
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ * Return: 0
+ */
 struct Dog
 {
 	char *name;
